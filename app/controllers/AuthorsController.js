0b3c55506ba589b4b71980ce6e2dcf1f00972c43
Nando <fernandosilva@unochapeco.edu.br>
2019-04-09 21:28:45 -0300

    add entities

--- app/controllers/AuthorsController.js
+++ app/controllers/AuthorsController.js
@@ -1,36 +1,57 @@
 const express = require('express');
 const router = express.Router();
-const data = [
-    {"id":1, "first_name": "Master", "last_name": "Theylor"},
-    {"id":2, "first_name": "Ana", "last_name": "Santos"}
-]
+const models = require('../models');
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({extended: true}));
+router.use(bodyParser.json());
+
 
 //create
 router.post('/', function (req, res){
-    res.status(200).send("Create Action");
+    models.Author.create(req.body).then(
+        author => res.status(200).send(author)
+    ).catch(err => res.status(500).send(err))
 });
 
 //Get all
 router.get('/', function(req, res){
-    res.status(200).send(data);
+    models.Author.findAll().then(authors =>
+        res.status(200).send(authors)
+        )
 });
 
 //Find one by id
 router.get('/:id', function(req, res){
-    data.map(d => {
-        if(d.id == req.params.id)
-        res.status(200).send(d)
-    })
+    models.Author.findByPk(req.params.id)
+    .then(author => {
+        if (!author) {
+            res.status(404).send("NOT FOUND")
+        }
+
+        res.status(200).send(author)
+    }).catch(err => res.status(500).send(err))
 });
 
 //Update
 router.put('/:id', function(req, res){
-    res.status(200).send("Update");
+    models.Author.findByPk(req.params.id).then(author => {
+        if (!author) {
+            res.status(404).send("NOT FOUND")
+        }
+
+        author.update(req.body);
+        res.status(200).send(author)
+    })
 });
 
 //Delete
 router.delete('/:id', function(req, res){
-    res.status(200).send("Delete");
+    models.Author.destroy({
+        where: {id: req.params.id}
+    }).the(author => {
+        res.status(200).send('Excluído com sucesso!')
+    })
 });
 
 module.exports = router;
